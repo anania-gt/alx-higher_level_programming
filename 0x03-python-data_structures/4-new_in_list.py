7cc673b0704e2e52f5e66aa29402db70499ae287
anania-gt <144389858+anania-gt@users.noreply.github.com>
2024-05-23 08:28:23 -0400

    Update 4-new_in_list.py may23

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,13 +4,13 @@ Replace an element in a list at a specific position without
 modifying the original list
 """
 def new_in_list(my_list, idx, element):
-    if idx<0 or idx>=len(my_list): 
-        new_list = my_list.copy()
-        print("{}".format(new_list))
-    else:
-        new_list=my_list.copy()
+    # Make a copy of the original list
+    new_list = my_list[:]
+    
+    # Check if the index is within the valid range
+    if 0 <= idx < len(my_list):
+        # Replace the element at the specified index
         new_list[idx] = element
-        print("{}".format(new_list))
-
-my_list = [1, 2, 3, 4, 5]
-new_in_list(my_list,8,10)
+    
+    # Return the new list
+    return new_list
